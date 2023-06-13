43cc3708fc15b34a6631ea49041249730d2aadf0
motivate23a@gmail.com <motivate23a@gmail.com>
2023-06-13 01:44:57 -0700

    Finishing of the major template template in the project

--- meroLms/meroLms/urls.py
+++ meroLms/meroLms/urls.py
@@ -6,4 +6,12 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 
     path('base',views.BASE, name='base'),
+
+    path('',views.HOME, name="home"),
+    path('about',views.ABOUT, name = "about"),
+    path('contact',views.CONTACT, name = "contact"),
+    path('blog', views.BLOG, name="blog"),
+    path('courses', views.COURSES, name="courses"),
+    path('singlecourses', views.SINGLE_COURSES, name="single_courses"),
+    path('singleblog', views.SINGLE_BLOG, name="single_blog"),
 ]
--- meroLms/meroLms/views.py
+++ meroLms/meroLms/views.py
@@ -3,3 +3,26 @@ from django.shortcuts import redirect,render
 
 def BASE(request):
     return render(request,'base.html')
+
+def HOME(request):
+    return render(request, 'main/home.html')
+
+
+def ABOUT(request):
+    return render(request, 'main/about.html')
+
+def CONTACT(request):
+    return render(request, 'main/contact.html')
+
+def COURSES(request):
+    return render(request, 'main/courses.html')
+
+def BLOG(request):
+    return render(request, 'main/blog.html')
+
+def SINGLE_COURSES(request):
+    return render(request, 'main/singlecourse.html')
+
+
+def SINGLE_BLOG(request):
+    return render(request, 'main/singleblog.html')
