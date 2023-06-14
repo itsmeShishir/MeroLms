c14b8083fd3ed31e3f94ac3ef0dba51365d3f319
motivate23a@gmail.com <motivate23a@gmail.com>
2023-06-14 05:57:14 -0700

    Finish of the admin category model

--- meroLms/app/models.py
+++ meroLms/app/models.py
@@ -1,4 +1,11 @@
 from django.db import models
 
 # Create your models here.
+# creating the models for category
+class Categories(models.Model):
+    icon = models.CharField(max_length=200,null=True)
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
 
