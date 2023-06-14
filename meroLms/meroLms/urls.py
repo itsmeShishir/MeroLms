65a6beabc7f38bafc6dbe356de773904a6b248e5
motivate23a@gmail.com <motivate23a@gmail.com>
2023-06-13 19:57:51 -0700

    IMporting the auth login and register page and making the user_login and add login throught custome django admin

--- meroLms/meroLms/urls.py
+++ meroLms/meroLms/urls.py
@@ -1,7 +1,7 @@
 
 from django.contrib import admin
-from django.urls import path
-from .import views
+from django.urls import path, include
+from .import views, user_login
 urlpatterns = [
     path('admin/', admin.site.urls),
 
@@ -14,4 +14,9 @@ urlpatterns = [
     path('courses', views.COURSES, name="courses"),
     path('singlecourses', views.SINGLE_COURSES, name="single_courses"),
     path('singleblog', views.SINGLE_BLOG, name="single_blog"),
+
+#     for the login and register path for the user
+    path('accounts/', include('django.contrib.auth.urls')),
+#     for the register user
+    path('accounts/register',user_login.REGISTER, name='register')
 ]
