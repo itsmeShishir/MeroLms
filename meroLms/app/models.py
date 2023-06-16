7be1255c46189e343cd402d7f7a4d2c75a34992b
motivate23a@gmail.com <motivate23a@gmail.com>
2023-06-15 18:53:18 -0700

    Finish of the category model where user can add the category with the name and icon and shown in the frontend part of home section

--- meroLms/app/models.py
+++ meroLms/app/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-
 # Create your models here.
 # creating the models for category
 class Categories(models.Model):
--- meroLms/meroLms/views.py
+++ meroLms/meroLms/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import redirect,render
+from app.models import Categories
 
 
 def BASE(request):
     return render(request,'base.html')
 
 def HOME(request):
-    return render(request, 'main/home.html')
+    category = Categories.objects.all().order_by('id')[0:5]
+    context ={
+        'category': category,
+    }
+    return render(request, 'main/home.html',context)
 
 
 def ABOUT(request):
